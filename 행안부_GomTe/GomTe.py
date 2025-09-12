24ac5973fc14b5f3ba71c78e797452906e357942
뚱이 <146941970+RomanticBear@users.noreply.github.com>
2025-09-12 13:30:15 +0900

    이코테 구현 재활

--- 행안부_GomTe/GomTe.py
+++ 행안부_GomTe/GomTe.py
@@ -94,3 +94,115 @@ for i in range(len(arr)):
 print(ans)
 
 '''
+
+
+
+# AVATA
+
+# 상하좌우
+
+'''
+N=int(input()) # 지도 크기
+
+lst=list(input().split())  # 이동 방향
+
+dir={'R':(0,1),'L':(0,-1),'U':(-1,0),'D':(1,0)} 
+
+x,y=0,0  # 시작점 
+
+for d in lst:
+    dx,dy=dir[d]
+
+    nx=x+dx
+    ny=y+dy
+
+    if (0<=nx<N) and (0<=ny<N):
+        x,y=nx,ny
+
+ans_x,ans_y=x+1,y+1
+# print(ans_x,ans_y)
+
+'''
+
+# 시각
+
+'''
+3이 총 몇번 들어갔는지 확인하기
+
+- 3이 하나라도 포함되어 있으면 카운트, 하나라도 없으면 노카운트
+
+00시 00분 00초 ~ N시 59분 59초
+
+'''
+
+'''
+H=int(input())
+cnt=0
+
+for i in range(H+1):
+    for j in range(60):
+        for k in range(60):
+            if ('3' in str(i)) or ('3' in str(j)) or ('3' in str(k)):
+                cnt+=1
+
+
+print(cnt)
+
+'''
+
+
+# 왕실의 나이트
+
+'''
+idx=input()
+lst=list(idx)
+
+row_idx={'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8}
+
+x=row_idx[lst[0]]
+y=int(lst[1])
+
+
+dxy=[(-2,1),(-2,-1),(2,1),(2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]
+
+ans=0
+for dir in dxy:
+    dx,dy=dir[0],dir[1]
+    nx,ny=x+dx,y+dy
+
+    if (1<=nx<9) and (1<=ny<9):
+        ans+=1
+
+
+print(ans)
+
+    
+'''
+
+
+# 문자열 재정렬
+
+'''
+lst=list(input())
+lst.sort()
+
+print(lst)
+
+num_lst=[]
+ch_lst=[]
+
+for i in range(len(lst)):
+    if ord('1')<=ord(lst[i])<=ord('9'):
+        num_lst.append(lst[i])
+    else:
+        ch_lst.append(lst[i])
+
+
+num_ans=sum(list(map(int,num_lst)))
+ch_ans=''.join(ch_lst)
+
+ans=ch_ans+str(num_ans)
+
+print(ans)
+
+'''
