552cac87a13c5b7c64889bfcf5ead16a2f49ae3b
뚱이 <146941970+RomanticBear@users.noreply.github.com>
2025-10-13 23:13:43 +0900

    N과 M(1)

--- 행안부_GomTe/GomTe.py
+++ 행안부_GomTe/GomTe.py
@@ -722,3 +722,30 @@ def back(cur_idx,sub_lst):
 back(0,[])
 
 """
+
+# 2차
+
+'''
+N,M=map(int,input().split())
+lst=[]
+v=[True]*(N+1)
+
+def dfs(sub_lst):
+    if len(sub_lst)==M:
+        lst.append(sub_lst[:])
+        return
+    
+    for num in range(1,N+1):
+        if v[num]:
+            v[num]=False
+            sub_lst.append(num)
+            dfs(sub_lst)
+            v[num]=True
+            sub_lst.pop()
+
+dfs([])
+
+for seq in lst:
+    print(*seq)
+
+'''
