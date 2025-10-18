b098ef2b0897b594436bcf60ecd1e3bd5d00961d
뚱이 <146941970+RomanticBear@users.noreply.github.com>
2025-10-18 18:48:33 +0900

    BOJ 14889 스타트와 링크 풀이 개선

--- 행안부_GomTe/GomTe.py
+++ 행안부_GomTe/GomTe.py
@@ -1192,6 +1192,7 @@ dfs(0,[])
 '''
 
 
+
 # 14889 스타트와 링크
 
 '''
@@ -1200,8 +1201,7 @@ dfs(0,[])
 
 '''
 
-
-
+'''
 def power_dfs(team,sub_lst):
 
     if len(sub_lst)==2:
@@ -1213,7 +1213,6 @@ def power_dfs(team,sub_lst):
             v[num]=False
             power_dfs(team,sub_lst+[num])
             v[num]=True
-
 
 
 # team_dfs 고민 포인트
@@ -1236,7 +1235,6 @@ def power_dfs(team,sub_lst):
             v[num]=True
 
     return total_scr
-
 
 
 # team_dfs 고민 포인트 
@@ -1266,14 +1264,12 @@ def team_dfs(idx,sub_lst):
             P_teamA=power_dfs(teamA,[])
             P_teamB=power_dfs(teamB,[])
 
-
             ans=min(ans,abs(P_teamA-P_teamB))
 
         return
     
     team_dfs(idx+1,sub_lst+[lst[idx]])
     team_dfs(idx+1,sub_lst)
-
 
 
 # main
@@ -1287,4 +1283,59 @@ team_dfs(0,[])
 print(ans)
 
 
+'''
 
+
+# team_dfs -> for문 변환 
+
+'''
+def power_dfs(team):
+
+    P_score=0
+    
+    for i in range(len(team)-1):
+        for j in range(i+1,len(team)):
+            P_score+=arr[team[i]][team[j]]+arr[team[j]][team[i]]
+
+    return P_score
+
+
+def team_dfs(idx,sub_lst):
+
+    global ans
+
+    # 가지치기 : 현재 뽑은 인원으로, 더 진행해봤자 절반에 해당하는 인원을 채울 수 없는 경우
+    if len(sub_lst)+(len(lst)-idx)<len(lst)//2:
+        return
+
+    if len(sub_lst)==len(lst)//2 :
+        if mark not in sub_lst:
+            return
+        
+        else:
+            teamA=[num for num in sub_lst]
+            teamB=[num for num in lst if num not in sub_lst]
+            # print(teamA,'|',teamB)
+
+            P_teamA=power_dfs(teamA)
+            P_teamB=power_dfs(teamB)
+
+            ans=min(ans,abs(P_teamA-P_teamB))
+
+        return
+    
+    team_dfs(idx+1,sub_lst+[lst[idx]])
+    team_dfs(idx+1,sub_lst)
+
+
+# main
+N=int(input())
+lst=[num for num in range(N)]
+arr=[list(map(int,input().split())) for _ in range(N)]
+ans=1e9
+mark=0
+team_dfs(0,[])
+print(ans)
+
+
+'''
