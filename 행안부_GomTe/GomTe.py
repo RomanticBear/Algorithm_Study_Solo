95d4d49c934bb2ad77d4e83f8b4eebaa3eac049c
뚱이 <146941970+RomanticBear@users.noreply.github.com>
2025-10-28 18:33:31 +0900

    BOJ 1914 하노이탑

--- 행안부_GomTe/GomTe.py
+++ 행안부_GomTe/GomTe.py
@@ -1532,6 +1532,7 @@ hanoi(첫번째 인자, 두번째 인자, 세번째 인자)
 
 '''
 
+'''
 def hanoi(n,A,B,C):
 
     # 가장 작은 원판(n=1)을 이동 시켜야하는 상황이라면, 
@@ -1552,5 +1553,41 @@ def hanoi(n,A,B,C):
 
 
 hanoi(3, 'A', 'B', 'C')
+
+'''
+
+
+# 1914 하노이 탑
+
+def hanoi(n,A,B,C):
+    
+    global depth
+    global lst
+
+    if n==1:
+        depth+=1
+        lst.append((A,C))
+        return
+    
+    hanoi(n-1,A,C,B)
+    depth+=1
+    lst.append((A,C)) # n-1 판 해결, n번째 판 이동
+
+    hanoi(n-1,B,A,C) # temp로 이동시킨 n-1판에 대한 이동
+
+
+# main 
+N=int(input())
+depth=0
+lst=[]
+
+
+if N<=20:
+    hanoi(N,1,2,3)
+    print(depth)
+    for start,end in lst:
+        print(start,end)
+else:
+    print((1<<N)-1)
 
 
